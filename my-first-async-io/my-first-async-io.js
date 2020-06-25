056bc695628e940538ba3ff4ef174d81416fb990
Phillip Tran <ptrandev@protonmail.com>
2020-06-24 22:57:00 -0400

    finished my first async io

---
+++ my-first-async-io/my-first-async-io.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+fs.readFile(process.argv[2], function (error, buffer) {
+  if (error) return console.error(error);
+
+  const lines = buffer.toString().split("\n").length - 1;
+  console.log(lines);
+});

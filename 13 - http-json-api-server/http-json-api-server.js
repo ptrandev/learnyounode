a40f7afca61f0e029752e52b9c333010b5a9d085
Phillip Tran <ptrandev@protonmail.com>
2020-06-26 00:24:52 -0400

    finished http json api server

---
+++ 13 - http-json-api-server/http-json-api-server.js
@@ -0,0 +1,31 @@
+const http = require('http')
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime: time.getTime() }
+}
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(req.url, 'http://example.com')
+  const time = new Date(parsedUrl.searchParams.get('iso'))
+
+  if (/^\/api\/parsetime/.test(req.url)) {
+    result = parsetime(time)
+  } else if (/^\/api\/unixtime/.test(req.url)) {
+    result = unixtime(time)
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(result))
+  }
+})
+
+server.listen(Number(process.argv[2]))

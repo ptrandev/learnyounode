4abd6f67567091fbb0552b0430b7b6b4f7ab3a91
Phillip Tran <ptrandev@protonmail.com>
2020-06-25 23:22:46 -0400

    finished juggling async

---
+++ 9 - juggling-async.js/juggling-async.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const bl = require("bl");
+
+let results = [];
+let count = 0;
+
+function printResults() {
+  for (let i = 0; i < 3; i++) console.log(results[i]);
+}
+
+function httpGet(index) {
+  http.get(process.argv[2 + index], (response) => {
+    response.pipe(
+      bl(function (err, data) {
+        if (err) return console.error(err);
+
+        results[index] = data.toString();
+        count++;
+
+        if (count == 3) printResults();
+      })
+    );
+  });
+}
+
+for (let i = 0; i < 3; i++) httpGet(i);
